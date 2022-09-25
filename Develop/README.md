c4a6026282f6483cb7bc32362a93d1e390e6e0dd
MadelineRamos <maddieeramos@gmail.com>
2022-09-25 14:40:18 -0400

    finished assignemnt

---
+++ Develop/README.md
@@ -0,0 +1,45 @@
+
+  # project
+
+  ## Description
+
+  ![](https://img.shields.io/badge/license-APACHE2.0-green)
+
+  skjdfhsdkljfh
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+    npm 1
+
+  ## Usage
+
+  fsdkljfs;lakdf
+
+  ## License
+
+  This project is licensed under the [APACHE 2.0](https://opensource.org/licenses/Apache-2.0) license.
+
+  ## Contributing
+
+  s.ladkfjs;aldkf
+
+  ## Tests
+
+  To run tests, run the following command:
+
+    npm test
+
+  ## Questions
+
+  If you have any questions about the repo, open and issue or contact me directly at maddie. You can find more of my work at [maddie](https://github.com/maddie)
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,97 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    var str = license;
+    str = str.replaceAll(' ', '');
+    return `![](https://img.shields.io/badge/license-${str}-green)`;
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+ if (license) {
+  var licenseLink = '';
+  switch (license) {
+    case 'APACHE 2.0':
+      licenseLink = 'Apache-2.0';
+      break;
+    case 'GPL 3.0':
+      licenseLink = 'GPL-3.0';
+      break;
+    case 'BSD 3':
+      licenseLink = 'BSD-3-Clause';
+      break;
+    default:
+      licenseLink = 'MIT';
+  }
+
+  return `https://opensource.org/licenses/${licenseLink}`;
+ } else {
+  return ''
+ }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `This project is licensed under the [${license}](${renderLicenseLink(license)}) license.`;
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+    ${data.install}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  To run tests, run the following command:
+
+    ${data.test}
+
+  ## Questions
+
+  If you have any questions about the repo, open and issue or contact me directly at ${data.email}. You can find more of my work at [${data.username}](https://github.com/${data.username})
 `;
 }
 
